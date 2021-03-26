c9098430c94d07cb975abd11c938214cc851aa39
Nirmalya Misra <39618712+nirmalya8@users.noreply.github.com>
2021-03-26 12:13:47 +0530

    Added Collatz Version 1 and 2

--- Collatz(Version 2).py
+++ Collatz(Version 2).py
@@ -1,4 +1,19 @@
-import random
+'''
+Version 2 of Collatz
+
+What is Collatz?
+Write a function named collatz() that has one parameter named number. If
+number is even, then collatz() should print number // 2 and return this value.
+If number is odd, then collatz() should print and return 3 * number + 1.
+Then write a program that lets the user type in an integer and that keeps
+calling collatz() on that number until the function returns the value 1.
+
+This version:
+In this version of Collatz, I have used Recursion in the Collatz function and I
+have validated the input by using the try and except block. As I have used recursion,
+I didn't have to write an external loop calling the Collatz function again and again
+until it returns 1. 
+'''
 def collatz(n):
     if n==1:
         return
